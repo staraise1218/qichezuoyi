cb09308bced8790ffa2b247c2314194177e7c29f
shengli <363912809@qq.com>
2020-01-31 21:52:47 +0800

    new

--- application/api/controller/Form.php
+++ application/api/controller/Form.php
@@ -5,6 +5,11 @@ namespace app\api\controller;
 use app\common\controller\Api;
 use app\api\library\Character;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use PhpOffice\PhpSpreadsheet\Reader\Xls;
+use PhpOffice\PhpSpreadsheet\Reader\Csv;
+
 /**
  * 首页接口
  */
@@ -83,6 +88,116 @@ class Form extends Api
 
 
         $this->success('请求成功', $data);
+    }
+
+    // 获取已提交的数据
+    public function getFormList(){
+        $user_id = input('user_id', 0);
+
+        $list = db('form_data')->where('user_id', $user_id)
+            ->field('id, name, age, sex, height, weight, shape, chair_hardness_cushion')
+            ->select();
+
+        $this->success('请求成功', $list);
+    }
+
+
+    // 提交表单
+    public function FormSubmit(){
+        $postData = input('post.');
+
+        if( db('form_data')->insert($postData)){
+            $this->success('提交成功');
+        } else {
+            $this->error('提交失败');
+        }
+        
+    }
+
+    //  查看表单结果
+    public function getResult(){
+        $id = input('id');
+
+        $formData = db('form_data')->where('id', $id)->find();
+        $formData['carType'] = db('car_type')->where('id', $formData['car_type_id'])->value('name'); // 车型
+        $formData['chairColor'] = db('chair_color')->where('id', $formData['chair_color_id'])->value('name'); // 座椅颜色
+        $formData['chairMaterial'] = db('chair_material')->where('id', $formData['chair_material_id'])->value('name'); // 座椅材质
+
+        if($formData['file'] == '') $this->error('缺少文件');
+
+
+        $filepath = '.'.$formData['file'];
+
+        $ext = pathinfo($filepath, PATHINFO_EXTENSION);
+        if (!in_array($ext, ['csv', 'xls', 'xlsx'])) {
+            $this->error('不支持的格式');
+        }
+
+        if ($ext === 'csv') {
+            $reader = new Csv();
+        } elseif ($ext === 'xls') {
+            $reader = new Xls();
+        } else {
+            $reader = new Xlsx();
+        }
+
+        if (!$PHPExcel = $reader->load($filepath)) {
+            $this->error(__('Unknown data format'));
+        }
+
+        $currentSheet = $PHPExcel->getSheet(0);  //读取文件中的第一个工作表
+        $columns = $currentSheet->getCell('B11')->getValue(); // B11 列
+
+        // $columns == 64 说明文件里上部分是 靠背数据 ，下部分是坐垫数据
+
+        // 靠背值
+        $backrestData = array(
+            'peak_pressure' => $currentSheet->getCell('B17')->getValue(), // 最大压力
+            'average_pressure' => $currentSheet->getCell('B16')->getValue(), // 平均压力
+            'contact_area' => $currentSheet->getCell('B19')->getValue(), // 接触面积
+        );
+        // 坐垫值
+        $cushionData = array(
+            'peak_pressure' => $currentSheet->getCell('B70')->getValue(), // 最大压力
+            'average_pressure' => $currentSheet->getCell('B69')->getValue(), // 平均压力
+            'contact_area' => $currentSheet->getCell('B72')->getValue(), // 接触面积
+        );
+
+        $backrestArr = $cushiontArr = [];
+        // 靠背硬度值数组
+        for ($currentRow = 21; $currentRow <= 60; $currentRow++) {
+            for ($currentColumn = 1; $currentColumn <= 64; $currentColumn++) {
+                $val = $currentSheet->getCellByColumnAndRow($currentColumn, $currentRow)->getValue();
+                if($val != 0) $backrestArr[] = $val;
+            }
+        }
+        // 靠背加载力 加载力：非零值的总和*非零值个数*1.27*1.27
+        $backrest_jiazaili = round(array_sum($backrestArr)*count($backrestArr)*1.27*1.27, 2);
+
+        // 坐垫硬度值数组
+        for ($currentRow = 74; $currentRow <= 113; $currentRow++) {
+            for ($currentColumn = 1; $currentColumn <= 40; $currentColumn++) {
+                $val = $currentSheet->getCellByColumnAndRow($currentColumn, $currentRow)->getValue();
+                if($val != 0) $cushiontArr[] = $val;
+            }
+        }
+        // 坐垫加载力
+        $cushion_jiazaili = round(array_sum($cushiontArr)*count($cushiontArr)*1.27*1.27, 2);
+
+        // 平均硬度， 该款座椅硬度方面XX（较软/偏硬，根据平均硬度评判）: 取坐垫硬度数值求平均数 与 4.1比较 
+        $average_hardness = $columns == 64 ? array_sum($cushiontArr)/count($cushiontArr) : array_sum($backrestArr)/count($backrestArr);
+        $average_hardness = $average_hardness > 4.1 ? '较硬' : '较软';
+        
+
+        $result['formData'] = $formData;
+        $result['result'] = array(
+            'backrestData' => $columns == 64 ? $backrestData : $cushionData,
+            'cushionData' => $columns == 64 ? $cushionData : $backrestData,
+            'backrest_jiazaili' => $columns == 64 ? $backrest_jiazaili : $cushion_jiazaili, // 靠背加载力
+            'cushion_jiazaili' => $columns == 64 ? $cushion_jiazaili : $backrest_jiazaili, // 坐垫加载力,
+            'average_hardness' => $average_hardness,
+        );
+        $this->success('', $result);
     }
 
     // 获得车型
